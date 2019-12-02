e273a265ccffa569ec09c79322292dc0fadda74a
Creditor <cred1tor@yandex.ru>
2019-12-02 19:24:46 +0600

    made parity check a separate function

--- src/games/even.js
+++ src/games/even.js
@@ -1,12 +1,14 @@
 import * as _ from 'lodash';
 
+const isEven = (number) => number % 2 === 0;
+
 export default (message) => {
   switch (message) {
     case 'gameObjective':
       return 'Answer "yes" if the number is even, otherwise answer "no".';
     case 'questionAndAnswer': {
       const number = _.random(100);
-      const answer = number % 2 === 0 ? 'yes' : 'no';
+      const answer = isEven(number) ? 'yes' : 'no';
       return [number, answer];
     }
     default:
