10187860e4215a130cb2e99e478e010e834ca90c
Creditor <cred1tor@yandex.ru>
2019-12-11 09:09:02 +0600

    removed the for cycle, implemented a recursive, iterative process

--- src/index.js
+++ src/index.js
@@ -8,27 +8,24 @@ export default (gameObjective, getQuestionAndAnswer) => {
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}\n`);
 
-  const playOneTurn = () => {
+  const playGame = (turnsLeft) => {
+    if (turnsLeft === 0) {
+      console.log(`Congratulations, ${userName}!`);
+      return true;
+    }
+
     const [question, correctAnswer] = getQuestionAndAnswer();
     const userAnswer = readlineSync.question(`Question: ${question}\nYour answer: `);
 
     if (userAnswer === correctAnswer) {
       console.log('Correct!');
-      return true;
+      return playGame(turnsLeft - 1);
     }
 
     console.log(`'${userAnswer}' is a wrong answer ;( Correct answer was '${correctAnswer}'.`);
+    console.log(`Let's try again, ${userName}`);
     return false;
   };
 
-  for (let i = turnsCount; i > 0; i -= 1) {
-    const outcome = playOneTurn();
-    if (!outcome) {
-      console.log(`Let's try again, ${userName}`);
-      return false;
-    }
-  }
-
-  console.log(`Congratulations, ${userName}!`);
-  return true;
+  return playGame(turnsCount);
 };
